fa57bf0f701282c721942082f1a00db6db4d84d5
Darshan L <darshanl1254@gmail.com>
2025-03-06 21:02:40 +0530

    Employee Repository using Spring data Jpa

---
+++ src/main/java/org/bluetree/emp_crud/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package org.bluetree.emp_crud.repository;
+
+import org.bluetree.emp_crud.dto.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
